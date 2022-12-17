569cd4bf19cbfa5f6a2177e402f4523eff0b446c
Roman Titov <ffuugoo@users.noreply.github.com>
2022-12-17 22:36:12 +0100

    Refactor implementation and extend it to support 8-bit and 128-bit blocks

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{mem, ops};
+use std::{fmt, mem, ops::BitXor};
 
 use num_traits::{
     ops::wrapping::{WrappingAdd, WrappingSub},
@@ -10,16 +10,19 @@ pub struct RC5<T> {
     rounds: u8,
 }
 
-impl<T: Block> RC5<T> {
+impl<'a, T: Block<'a>> RC5<T>
+where
+    TryFromU8SliceError<'a, T::Bytes>: fmt::Debug,
+{
     pub fn new(key: &[u8], rounds: u8) -> Self {
-        assert!(!key.is_empty());
-        assert_eq!(key.len() % T::BYTES, 0);
+        assert!(!key.is_empty(), "key is empty");
+        assert_eq!(key.len() % T::bytes(), 0, "key length have to be a multiple of word length");
 
         // Split `key` into `words`
-        let mut words = vec![T::default(); key.len() / T::BYTES];
+        let mut words = vec![T::default(); key.len() / T::bytes()];
 
         for byte in (0..key.len()).rev() {
-            let word = byte / T::BYTES;
+            let word = byte / T::bytes();
 
             words[word] = words[word]
                 .rotate_left(8)
@@ -63,13 +66,17 @@ impl<T: Block> RC5<T> {
         Self { subkey, rounds }
     }
 
-    pub fn encode(&self, plaintext: &[u8]) -> Vec<u8> {
-        assert_eq!(plaintext.len(), T::BYTES * 2);
+    pub fn encode(&self, plaintext: &'a [u8]) -> Vec<u8> {
+        assert_eq!(
+            plaintext.len(),
+            T::bytes() * 2,
+            "plaintext length have to be exactly two words",
+        );
 
-        let (first, second) = plaintext.split_at(T::BYTES);
+        let (first, second) = plaintext.split_at(T::bytes());
 
-        let mut first = T::from_le_bytes(first).wrapping_add(&self.subkey[0]);
-        let mut second = T::from_le_bytes(second).wrapping_add(&self.subkey[1]);
+        let mut first = T::from_le_bytes_slice(first).wrapping_add(&self.subkey[0]);
+        let mut second = T::from_le_bytes_slice(second).wrapping_add(&self.subkey[1]);
 
         for round in 1..=self.rounds as usize {
             first = (first ^ second)
@@ -81,16 +88,20 @@ impl<T: Block> RC5<T> {
                 .wrapping_add(&self.subkey[round * 2 + 1]);
         }
 
-        into_bytes(first, second)
+        first.to_le_bytes_vec(second)
     }
 
-    pub fn decode(&self, ciphertext: &[u8]) -> Vec<u8> {
-        assert_eq!(ciphertext.len(), T::BYTES * 2);
+    pub fn decode(&self, ciphertext: &'a [u8]) -> Vec<u8> {
+        assert_eq!(
+            ciphertext.len(),
+            T::bytes() * 2,
+            "ciphertext length have to be exactly two words",
+        );
 
-        let (first, second) = ciphertext.split_at(T::BYTES);
+        let (first, second) = ciphertext.split_at(T::bytes());
 
-        let mut first = T::from_le_bytes(first);
-        let mut second = T::from_le_bytes(second);
+        let mut first = T::from_le_bytes_slice(first);
+        let mut second = T::from_le_bytes_slice(second);
 
         for round in (1..=self.rounds as usize).rev() {
             second = second
@@ -105,88 +116,72 @@ impl<T: Block> RC5<T> {
         first = first.wrapping_sub(&self.subkey[0]);
         second = second.wrapping_sub(&self.subkey[1]);
 
-        into_bytes(first, second)
+        first.to_le_bytes_vec(second)
     }
 }
 
-fn into_bytes(first: impl Block, second: impl Block) -> Vec<u8> {
-    first.to_le_bytes().into_iter().chain(second.to_le_bytes()).collect()
-}
-
-pub trait Block:
+pub trait Block<'a>:
     Default
     + PrimInt
     + WrappingAdd
     + WrappingSub
     + UniformRotate
-    + ops::BitXor
+    + BitXor
     + From<u8>
     + Sized
 {
-    const BYTES: usize = mem::size_of::<Self>();
-
     const MAGIC_P: Self;
     const MAGIC_Q: Self;
 
-    type ToLeBytesOutput: IntoIterator<Item = u8>;
+    type Bytes: TryFrom<&'a [u8]> + AsRef<[u8]>;
 
-    fn from_le_bytes(bytes: &[u8]) -> Self;
-    fn to_le_bytes(self) -> Self::ToLeBytesOutput;
+    fn from_le_bytes(bytes: Self::Bytes) -> Self;
+    fn to_le_bytes(self) -> Self::Bytes;
+
+    fn bytes() -> usize {
+        mem::size_of::<Self>()
+    }
+
+    fn from_le_bytes_slice(bytes: &'a [u8]) -> Self
+    where
+        TryFromU8SliceError<'a, Self::Bytes>: fmt::Debug,
+    {
+        Self::from_le_bytes(bytes.try_into().unwrap())
+    }
+
+    fn to_le_bytes_vec(self, other: Self) -> Vec<u8> {
+        let mut bytes = Vec::from(self.to_le_bytes().as_ref());
+        bytes.extend_from_slice(other.to_le_bytes().as_ref());
+        bytes
+    }
 }
 
-impl Block for u16 {
-    const MAGIC_P: Self = 0xB7E1;
-    const MAGIC_Q: Self = 0x9E37;
+pub type TryFromU8SliceError<'a, T> = <T as TryFrom<&'a [u8]>>::Error;
 
-    type ToLeBytesOutput = [u8; 2];
+macro_rules! block {
+    ($type:ty, $magic_p:literal, $magic_q:literal $(,)?) => {
+        impl<'a> Block<'a> for $type {
+            const MAGIC_P: Self = $magic_p;
+            const MAGIC_Q: Self = $magic_q;
 
-    fn from_le_bytes(bytes: &[u8]) -> Self {
-        match bytes {
-            &[b1, b2] => Self::from_le_bytes([b1, b2]),
-            _ => panic!(),
+            type Bytes = [u8; mem::size_of::<Self>()];
+
+            fn from_le_bytes(bytes: Self::Bytes) -> Self {
+                Self::from_le_bytes(bytes)
+            }
+
+            fn to_le_bytes(self) -> Self::Bytes {
+                self.to_le_bytes()
+            }
         }
-    }
-
-    fn to_le_bytes(self) -> Self::ToLeBytesOutput {
-        self.to_le_bytes()
-    }
+    };
 }
 
-impl Block for u32 {
-    const MAGIC_P: Self = 0xB7E15163;
-    const MAGIC_Q: Self = 0x9E3779B9;
-
-    type ToLeBytesOutput = [u8; 4];
-
-    fn from_le_bytes(bytes: &[u8]) -> Self {
-        match bytes {
-            &[b1, b2, b3, b4] => Self::from_le_bytes([b1, b2, b3, b4]),
-            _ => panic!(),
-        }
-    }
-
-    fn to_le_bytes(self) -> Self::ToLeBytesOutput {
-        self.to_le_bytes()
-    }
-}
-
-impl Block for u64 {
-    const MAGIC_P: Self = 0xB7E151628AED2A6B;
-    const MAGIC_Q: Self = 0x9E3779B97F4A7C15;
-
-    type ToLeBytesOutput = [u8; 8];
-
-    fn from_le_bytes(bytes: &[u8]) -> Self {
-        match bytes {
-            &[b1, b2, b3, b4, b5, b6, b7, b8] => Self::from_le_bytes([b1, b2, b3, b4, b5, b6, b7, b8]),
-            _ => panic!(),
-        }
-    }
-
-    fn to_le_bytes(self) -> Self::ToLeBytesOutput {
-        self.to_le_bytes()
-    }
-}
+block!(u8, 0xB7, 0x9F);
+block!(u16, 0xB7E1, 0x9E37);
+block!(u32, 0xB7E15163, 0x9E3779B9);
+block!(u64, 0xB7E151628AED2A6B, 0x9E3779B97F4A7C15);
+block!(u128, 0xB7E151628AED2A6ABF7158809CF4F3C7, 0x9E3779B97F4A7C15F39CC0605CEDC835);
 
 // RC5 algorithm rotates words "words times".
 //
@@ -217,7 +212,7 @@ pub trait NothrowShift {
 }
 
 macro_rules! rotate {
-    (@rotate $method:ident $first:ident $second:ident) => {
+    (@rotate $method:ident, $first:ident, $second:ident $(,)?) => {
         fn $method(self, other: Self) -> Self {
             let bits = mem::size_of::<Self>() * 8;
             let shift_by = other & (bits - 1) as Self;
@@ -225,7 +220,7 @@ macro_rules! rotate {
         }
     };
 
-    (@shift $method:ident $op:path) => {
+    (@shift $method:ident, $op:path $(,)?) => {
         fn $method(self, other: Self) -> Self {
             if (other as usize) < mem::size_of::<Self>() * 8 {
                 $op(self, other)
@@ -237,20 +232,22 @@ macro_rules! rotate {
 
     ($type:ty) => {
         impl UniformRotate for $type {
-            rotate!(@rotate uniform_rotate_left nothrow_shift_left nothrow_shift_right);
-            rotate!(@rotate uniform_rotate_right nothrow_shift_right nothrow_shift_left);
+            rotate!(@rotate uniform_rotate_left, nothrow_shift_left, nothrow_shift_right);
+            rotate!(@rotate uniform_rotate_right, nothrow_shift_right, nothrow_shift_left);
         }
 
         impl NothrowShift for $type {
-            rotate!(@shift nothrow_shift_left std::ops::Shl::shl);
-            rotate!(@shift nothrow_shift_right std::ops::Shr::shr);
+            rotate!(@shift nothrow_shift_left, std::ops::Shl::shl);
+            rotate!(@shift nothrow_shift_right, std::ops::Shr::shr);
         }
     };
 }
 
+rotate!(u8);
 rotate!(u16);
 rotate!(u32);
 rotate!(u64);
+rotate!(u128);
 
 #[cfg(test)]
 mod tests {
@@ -301,4 +298,114 @@ mod tests {
         let res = decode(key, ct);
         assert!(&pt[..] == &res[..]);
     }
+
+    macro_rules! test_rc5 {
+        (
+            $encode:ident,
+            $decode:ident,
+            $block:ty,
+            $rounds:expr,
+            $key:expr,
+            $pt:expr,
+            $ct:expr
+            $(,)?
+        ) => {
+            #[test]
+            fn $encode() {
+                const KEY: &[u8] = &$key;
+                const PT: &[u8] = &$pt;
+                const CT: &[u8] = &$ct;
+                let res = RC5::<$block>::new(KEY, $rounds).encode(PT);
+                assert_eq!(&CT[..], &res[..]);
+            }
+
+            #[test]
+            fn $decode() {
+                const KEY: &[u8] = &$key;
+                const PT: &[u8] = &$pt;
+                const CT: &[u8] = &$ct;
+                let res = RC5::<$block>::new(KEY, $rounds).decode(CT);
+                assert_eq!(&PT[..], &res[..]);
+            }
+        };
+    }
+
+    test_rc5!(
+        encode_rc5_8_12_4,
+        decode_rc5_8_12_4,
+        u8,
+        12,
+        [0x00, 0x01, 0x02, 0x03],
+        [0x00, 0x01],
+        [0x21, 0x2A],
+    );
+
+    test_rc5!(
+        encode_rc5_16_16_8,
+        decode_rc5_16_16_8,
+        u16,
+        16,
+        [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07],
+        [0x00, 0x01, 0x02, 0x03],
+        [0x23, 0xA8, 0xD7, 0x2E],
+    );
+
+    test_rc5!(
+        encode_rc5_32_20_16,
+        decode_rc5_32_20_16,
+        u32,
+        20,
+        [
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+        ],
+        [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07],
+        [0x2A, 0x0E, 0xDC, 0x0E, 0x94, 0x31, 0xFF, 0x73],
+    );
+
+    test_rc5!(
+        encode_rc5_64_24_24,
+        decode_rc5_64_24_24,
+        u64,
+        24,
+        [
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+        ],
+        [
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+        ],
+        [
+            0xA4, 0x67, 0x72, 0x82, 0x0E, 0xDB, 0xCE, 0x02,
+            0x35, 0xAB, 0xEA, 0x32, 0xAE, 0x71, 0x78, 0xDA,
+        ],
+    );
+
+    test_rc5!(
+        encode_rc5_128_28_32,
+        decode_rc5_128_28_32,
+        u128,
+        28,
+        [
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+            0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+
+        ],
+        [
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+            0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+        ],
+        [
+            0xEC, 0xA5, 0x91, 0x09, 0x21, 0xA4, 0xF4, 0xCF,
+            0xDD, 0x7A, 0xD7, 0xAD, 0x20, 0xA1, 0xFC, 0xBA,
+            0x06, 0x8E, 0xC7, 0xA7, 0xCD, 0x75, 0x2D, 0x68,
+            0xFE, 0x91, 0x4B, 0x7F, 0xE1, 0x80, 0xB4, 0x40,
+        ],
+    );
 }
